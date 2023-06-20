4860be9dded3b871769ec218a85d3454093ec9a4
sarthak2205 <nirmal.sarthak@gmail.com>
2023-06-20 12:50:24 +0530

    Final Commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import HomePage from "./pages/HomePage";
 
 function App() {
   return (
-    <div className="flex flex-col items-center p-6 space-y-6 bg-slate-300 min-h-screen w-full">
+    <div className="flex flex-col items-center p-2 md:p-6 space-y-6 bg-slate-300 min-h-screen w-screen">
       <HomePage />
     </div>
   );
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -54,30 +54,30 @@ const HomePage = () => {
 
 
   return (
-    <div className='py-12 flex flex-col items-center space-y-12 min-h-screen w-full'>
+    <div className='py-12 flex flex-col items-center space-y-12 min-h-screen w-screen px-5'>
         <h1 className='text-6xl font-extrabold text-indigo-700'>Sarthak's Short Url</h1>
         <form 
-            className='w-1/2 bg-white shadow-md rounded-md border p-6 flex flex-col items-center space-y-6'
+            className='w-full md:w-1/2 bg-white shadow-md rounded-md border p-6 flex flex-col items-center space-y-6'
             onSubmit={handleSubmit}
         >
-            <h1 className='text-3xl font-bold text-gray-800'>Enter the link here that is to be shortened:</h1>
+            <h1 className='text-xl md:text-3xl font-bold text-gray-800'>Enter the link here that is to be shortened:</h1>
             <input
                 placeholder='Enter the link'
-                className='border-2 rounded-md px-4 py-1 w-3/4' 
+                className='border-2 rounded-md px-4 py-1 md:w-3/4 w-full' 
                 onChange={(e) => setUrl(e.target.value)}
             />
             <button 
                 type='submit'
-                className='text-white bg-indigo-600 font-bold text-2xl rounded-full py-2 px-5'
+                className='text-white bg-indigo-600 font-bold text-xl md:text-2xl rounded-full py-2 px-5'
             >
                 Create
             </button>
         </form>
-        <div className='w-1/2 bg-white shadow-md rounded-md border p-6 flex flex-col items-center space-y-6'>
-            <h1 className='text-3xl font-bold text-gray-800'>Your Shortened URL: </h1>
+        <div className='w-full md:w-1/2 bg-white shadow-md rounded-md border p-6 flex flex-col items-center space-y-6'>
+            <h1 className='text-xl md:text-3xl font-bold text-gray-800'>Your Shortened URL: </h1>
             <input
                 placeholder='Enter the link'
-                className='border-2 rounded-md px-4 py-1 w-3/4' 
+                className='border-2 rounded-md px-4 py-1 md:w-3/4 w-full' 
                 value={Url ? `${inputValue}` : ''}
                 readOnly
             />
@@ -87,7 +87,7 @@ const HomePage = () => {
             <div className='flex flex-row justify-center items-start'>
                 <button 
                     type='submit'
-                    className={`text-white font-bold text-2xl rounded-full py-2 px-5 ${copied ? 'scale-105 duration-300 bg-purple-700': 'bg-indigo-600'}`}
+                    className={`text-white font-bold text-xl md:text-2xl rounded-full py-2 px-5 ${copied ? 'scale-105 duration-300 bg-purple-700': 'bg-indigo-600'}`}
                     onClick={copyToClipboard}
                 >
                     {copied ? "Copied" : "Copy URL"}
@@ -100,25 +100,25 @@ const HomePage = () => {
                 <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
                 <div className="overflow-hidden">
                     <table className="min-w-full text-center text-sm font-light">
-                    <thead className="border-b dark:border-neutral-500 font-extrabold text-xl">
+                    <thead className="border-b dark:border-neutral-500 font-extrabold text-base md:text-xl">
                         <tr>
                         <th scope="col" className="px-6 py-4">#</th>
                         <th scope="col" className="px-6 py-4">Link</th>
                         <th scope="col" className="px-6 py-4">Short ID</th>
-                        <th scope="col" className="px-6 py-4">Number of Clicks</th>
+                        <th scope="col" className="px-6 py-4 text-xs md:text-base flex">Number of Clicks</th>
                         </tr>
                     </thead>
                         {data.map((item, index) => (
                         <tbody key={item.id}>
                             <tr key={index+1} className={`border-b dark:border-neutral-500 text-sky-600 font-bold ${(index+1) % 2 === 1 ? 'bg-white' : 'bg-transparent'}`}>
                                 <td className="whitespace-nowrap px-6 py-4 font-medium text-black">{index+1}</td>
-                                <td className="whitespace-nowrap px-6 py-4"><a href={item.redirectURL} target="_blank" rel="noreferrer">{item.redirectURL}</a></td>
+                                <td className="whitespace-nowrap px-6 py-4 text-xs md:text-base"><a href={item.redirectURL} target="_blank" rel="noreferrer">{item.redirectURL}</a></td>
                                 <td className="whitespace-nowrap px-6 py-4"><a href={`http://localhost:8000/${item.shortId}`} target='_blank' rel="noreferrer">{`${item.shortId}`}</a></td>
                                 <td className="whitespace-nowrap px-6 py-4 text-black">{item.visitHistory.length}</td>
                             </tr>
                         </tbody>
                     ))}                  
-                    </table>
+                    </table>    
                 </div>
                 </div>
             </div>
